75ce5aa800331d8141428057c2f46f77d8059221
jhemysbarros <jhemysbarros@gmail.com>
2020-10-02 18:58:45 -0300

    Projeto de IA usando SpeechRecognition

---
+++ readAudio.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+PATH = 'audio.wav'
+
+arquivo = sr.Recognizer()
+    
+with sr.AudioFile(PATH) as source:
+
+ audio = arquivo.record(source)
+     
+try:
+        
+     fala = arquivo.recognize_google(audio,language='pt-BR')
+     
+     print(fala)
+        
+except sr.UnkownValueError:
+     print("Não entendi")
